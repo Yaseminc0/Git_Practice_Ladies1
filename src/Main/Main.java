45fc910cc8412b380654131a3ef9ef55fe416c64
Yaseminc0 <yasemincalik77@gmail.com>
2023-11-03 19:00:12 +0000

    task added to main file

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,24 @@
 package Main;
 
+import Yasemin.Week1_Task1;
+
 public class Main {
+    public static void main(String[] args) {
+
+        //space for rest of tasks
+        // test module issue
+
+
+        // String-2 > xyzThere (Yasemin)
+        System.out.println("String-2 > xyzThere");
+        System.out.println("Test case 1: ");
+        System.out.println(Week1_Task1.xyzThere("abcxyz"));
+        System.out.println("Test case 2:");
+        System.out.println(Week1_Task1.xyzThere("abc.xyz"));
+        System.out.println("Test case 3:");
+        System.out.println(Week1_Task1.xyzThere("xyz.abc"));
+
+
+
+    }
 }
