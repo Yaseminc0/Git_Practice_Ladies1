95226fce4d18d3db8391a7b28366c44a3ad05e80
Zehra <zbagcifr@gmail.com>
2023-11-03 20:36:25 +0100

    updated task1 class

--- src/zehra/Task1.java
+++ src/zehra/Task1.java
@@ -1,4 +1,21 @@
 package zehra;
 
 public class Task1 {
+    public static void main(String[] args) {
+        String isBob="qhb4b";
+        System.out.println(isBobHere(isBob));
+
+    }
+    public static boolean isBobHere(String str) {
+
+        boolean bob=false;
+        for(int i=0;i<str.length()-2;i++) {
+            if (str.substring(i, i + 1).equals("b") && str.substring(i + 2, i + 3).equals("b")) {
+                bob = true;
+            }
+        }
+
+
+        return bob;
+    }
 }
